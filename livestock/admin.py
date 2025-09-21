9af1a5f80e6d92ef3a31b3712b23e12a95c858c2
Hansraaj-Gavande8 <live.hans.live@gmail.com>
2025-09-21 16:52:53 +0530

    Add files via upload

---
+++ livestock/admin.py
@@ -0,0 +1,13 @@
+﻿from django.contrib import admin
+from .models import Farm, Livestock
+
+@admin.register(Farm)
+class FarmAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner']
+    search_fields = ['name']
+
+@admin.register(Livestock)
+class LivestockAdmin(admin.ModelAdmin):
+    list_display = ['tag_number', 'farm', 'species']
+    list_filter = ['species']
+    search_fields = ['tag_number']
---
+++ livestock/models.py
@@ -0,0 +1,20 @@
+﻿import uuid
+from django.db import models
+from django.contrib.auth.models import User
+
+class Farm(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Livestock(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
+    tag_number = models.CharField(max_length=50)
+    species = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.tag_number
